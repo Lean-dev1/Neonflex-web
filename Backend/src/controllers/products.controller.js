5500ea526d7098261799a07ab10810cfdf5fa2e2
Leandro jerez <ljerez@fi.uba.ar>
2026-02-17 14:01:01 -0300

    fix errores backen

--- Backend/src/controllers/products.controller.js
+++ Backend/src/controllers/products.controller.js
@@ -6,7 +6,7 @@ const streamifier = require('streamifier');
 const uploadToCloudinary = (buffer) => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
-      { folder: "cisneros_products" },
+      { folder: "neonflex_products" },
       (error, result) => {
         if (error) return reject(error);
         resolve(result);
